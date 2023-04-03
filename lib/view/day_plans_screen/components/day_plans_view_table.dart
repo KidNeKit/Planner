97af173327993ce3303e680e646894102cea391d
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-04-03 17:16:14 +0300

    Added first fiew of event table

--- lib/view/day_plans_screen/components/day_plans_view_table.dart
+++ lib/view/day_plans_screen/components/day_plans_view_table.dart
@@ -17,8 +17,120 @@ class DayPlansViewTable extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         }
-        return const Text('Table');
+        return LayoutBuilder(
+          builder: (ctx, constraints) {
+            double hourWidth = constraints.maxWidth;
+            double hourHeight = 0.07 * MediaQuery.of(context).size.height;
+            return Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: SingleChildScrollView(
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: List.generate(
+                            24,
+                            (index) => HourContainer(
+                                  index: index,
+                                  hourHeight: hourHeight,
+                                  hourWidth: hourWidth,
+                                )),
+                      ),
+                      Positioned(
+                        left: 0.15 * hourWidth,
+                        child: Container(
+                          height: 24 * hourHeight,
+                          width: 1,
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ),
+                      //todo add event here
+                      EventContainer(
+                          hours: 9,
+                          minutes: 45,
+                          containerHeight: hourHeight,
+                          containerWidth: hourWidth),
+                      EventContainer(
+                          hours: 6,
+                          minutes: 5,
+                          containerHeight: hourHeight,
+                          containerWidth: hourWidth),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
       },
+    );
+  }
+}
+
+class HourContainer extends StatelessWidget {
+  final int index;
+  final double hourHeight;
+  final double hourWidth;
+  const HourContainer(
+      {required this.index,
+      required this.hourHeight,
+      required this.hourWidth,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        double containerWidth = constraints.maxWidth;
+        return SizedBox(
+          height: hourHeight,
+          width: 0.15 * containerWidth,
+          child: Center(
+            child: Text(
+              '$index',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.black.withOpacity(0.5)),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class EventContainer extends StatelessWidget {
+  final double hours;
+  final double minutes;
+  final double containerHeight;
+  final double containerWidth;
+  const EventContainer(
+      {required this.hours,
+      required this.minutes,
+      required this.containerHeight,
+      required this.containerWidth,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: hours * containerHeight + minutes / 60 * containerHeight,
+      left: 0.15 * containerWidth,
+      child: Container(
+        height: containerHeight,
+        width: 0.85 * containerWidth,
+        color: Colors.amber.withOpacity(0.5),
+        child: Center(
+          child: Text(
+            'Event label',
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
+      ),
     );
   }
 }
